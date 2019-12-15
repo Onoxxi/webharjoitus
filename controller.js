72522d9c17717d0949a7fc4c3afa49a68b3ef03d
Arttu Tolonen <Arttu.Tolonen@edu.savonia.fi>
2019-12-16 00:30:33 +0200

    lisaysta

--- controller.js
+++ controller.js
@@ -1,18 +1,16 @@
 'use strict'
 
-
-// Asenna ensin mysql driver
-// npm install mysql --save
-
 var mysql = require('mysql');
 const querystring = require('querystring');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'root', // HUOM! Älä käytä root:n tunnusta tuotantokoneella!!!!
+  user     : 'root',
   password : 'salasana',
   database : 'chatpalvelu'
 });
+
+
 
 module.exports =
 {
@@ -23,6 +21,41 @@ module.exports =
         });
         console.log("Body = " + JSON.stringify(req.body));
         console.log("Params = " + JSON.stringify(req.query));
+
+    },
+
+    rekisteroi: function(req, res){
+      var data = req.params;
+
+        var sql = 'INSERT INTO kayttajat (nimi, salasana) VALUES' +
+        '("' + data.nimi + '", "' + data.salasana + '")'
+        connection.query(sql, function(error, results, fields){
+          console.log("Data = " + JSON.stringify(results));
+          res.json(results);
+        });
+        console.log("Body = " + JSON.stringify(req.body));
+        console.log("Params = " + JSON.stringify(req.query));
+
+    },
+
+    login: function(req, res){
+      var data = req.params;
+
+      var sql = 'SELECT kayttajaID FROM kayttajat WHERE nimi LIKE "' + data.nimi + '" AND salasana LIKE "' + data.salasana + '"'
+      connection.query(sql, function(error, results, fields){
+        if(results == ""){
+          console.log("Väärä salasana")
+          res.clearCookie("cookie_name");
+        }else {
+          console.log("lyöty");
+          res.cookie(data.nimi, results[0].kayttajaID);
+        }
+        console.log("Data = " + JSON.stringify(results));
+        res.json(results);
+      });
+
+      console.log("Body = " + JSON.stringify(req.body));
+      console.log("Params = " + JSON.stringify(req.query));
 
     }
 
--- server.js
+++ server.js
@@ -2,10 +2,17 @@ var express = require('express');
 var app=express();
 
 var bodyParser = require('body-parser');
-var customerController = require('./controller');
+var controller = require('./controller');
 
 const http = require('http');
 const url = require('url');
+
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+var redirect = require("express-redirect");
+redirect(app);
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3002;
@@ -28,15 +35,41 @@ app.use(bodyParser.json());
 
 
 // Staattiset filut
-app.use(express.static('webharjoitus'));
+app.use(express.static('js'));
 
 // REST API Asiakas
 app.route('/Types')
 
+app.route('/logi/:nimi/:salasana')
+  .get(controller.login);
 
-app.route('/Asiakas')
+app.route('/regi/:nimi/:salasana')
+  .post(controller.rekisteroi);
 
 app.get('/', function(request, response){
+  var keksi;
+  console.log("asd", request.cookies);
+  keksi = request.cookies;
+  console.log(keksi[0]);
+  if(keksi[0] == null){
+    console.log(keksi[0]);
+    response.sendFile('login.html', { root: __dirname + "/html" } );
+    console.log("login");
+  } else {
+    response.sendFile('main.html', { root: __dirname + "/html" } );
+    console.log("main");
+  }
+});
+
+app.get('/rekisteroi', function(request, response){
+  response.sendFile('rekist.html', { root: __dirname + "/html" } );
+});
+
+app.get('/login', function(request, response){
+  response.sendFile('login.html', { root: __dirname + "/html" } );
+});
+
+app.get('/chatti', function(request, response){
   response.sendFile('main.html', { root: __dirname + "/html" } );
 
 });
